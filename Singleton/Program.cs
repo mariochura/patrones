63e410f5c75505d039ebf499cb10d584d8ae8cd6
Mario Chura <mario.chura@uab.edu.bo>
2024-05-06 11:43:15 -0400

    subiendo el patron singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "Hola marte";
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+        }
+            
+    }
+}
+
+
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+        protected Singleton() { 
+            mensaje = "Hola mundo";
+        }
+        public static Singleton Instance
+        { 
+            get 
+            { 
+                if(instance == null)
+                    instance= new Singleton();
+
+                return instance; 
+            } 
+        }
+    }
+}
